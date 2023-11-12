b5ff9f63dd3192808494471a2e14c0ce72e42fad
Zo Alfiqar Salloum <zworkmail2023@gmail.com>
2023-11-12 10:54:41 +0200

    18

--- FCS-Midterm/Zoalfiqar_Salloum_Midterm_Solution.py
+++ FCS-Midterm/Zoalfiqar_Salloum_Midterm_Solution.py
@@ -1,15 +1,115 @@
 
 ###################################################################
 ###################################################################
-###################### FCS Midterm Project ########################
-###################### Zo Alfiqar Salloum #########################
-############### Advanced Browser Tabs Simulation ##################
-################### Friday, November 10, 2023 #####################
+############           FCS Midterm Project           ##############
+############           Zo Alfiqar Salloum           ###############
+#######          Advanced Browser Tabs Simulation           #######
+#######          Friday, November 10/11/12, 2023              #####
 ###################################################################
 ###################################################################
 import json
 from bs4 import BeautifulSoup
 import requests
+
+
+
+
+
+###################################################################
+###################################################################
+########### This function displays the software menu ##############
+###################################################################
+###################################################################
+def displayMenu():
+    print("Welcome to ABTS.\nThe most Advanced Browser Tabs Simulation.\nHere is the Menu, please choose wisely:\n1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tabs\n6. Clear All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit ABTS")
+    choice = input("Please enter your choice's number: ")
+    while (choice != "1") and (choice != "2") and (choice != "3") and (choice != "4") and (choice != "5") and (choice != "6") and (choice != "7") and (choice != "8") and (choice != "9"):
+        choice = input("Invalid input! Please enter one of the numbers shown: ")
+    choice = int(choice)
+    return choice
+
+
+
+
+
+###################################################################
+###################################################################
+###### This function asks the user to enter a tab's title, ########
+########## handles the user's input and return a title ############
+###################################################################
+###################################################################
+def getTitle():
+    title = input("Please enter the tab's title: ")
+    print("Are you sure that",title,"is the tab's title?\n1. YES\n2. NO")
+    confirm = input("Please type 1 or 2: ")
+    while (confirm != "1") and (confirm != "2"):
+        confirm = input("Invalid input! Please enter 1 or 2: ")
+    if confirm == "1":
+        return title
+    elif confirm == "2":
+        return getTitle()
+
+
+
+
+
+###################################################################
+###################################################################
+###### This function asks the user to enter the tab's URL, ########
+############ handles the user's input and return a URL ############
+############################### ## ################################
+##  NOTE: I did a research to know how to check if the user's    ##
+##  input is a valid URL. I found so many methods and finally    ##
+##  chose .startwith() method. Here is the link of the page:    ###
+##    https://www.geeksforgeeks.org/python-check-url-string/     ##
+###################################################################
+###################################################################
+def getURL():
+    url = input("Please enter a valid URL.\nNote: URL should start with 'http:', 'https:' or 'www.'\nType your URL here: ")
+    while (url.startswith("https:") != True) and (url.startswith("http:") != True) and (url.startswith("www.") != True):
+        url = input("Invalid URL!\nPlease enter a valid URL.\nNote: URL should start with 'http:', 'https:' or 'www.'\nType your URL here: ")
+    print("Are you sure that",url,"is the tab's URL?\n1. YES\n2. NO")
+    confirm = input("Please type 1 or 2: ")
+    while (confirm != "1") and (confirm != "2"):
+        confirm = input("Invalid input! Please enter 1 or 2: ")
+    if confirm == "1":
+        return url
+    elif confirm == "2":
+        return getURL()
+
+
+
+
+
+###################################################################
+###################################################################
+########## This function asks the user to enter an Index, #########
+########## handles the user's input and return the Index ##########
+###                 If no index is provided,                    ###
+###         the function will return (-1) as a value            ###
+###################################################################
+###################################################################
+def getIndex():
+    with open("opened_tabs.json") as f:
+        data = json.load(f)
+
+    index = input("Please enter the index: ")
+    if (index == ""):
+        index = -1
+        return index
+
+    if (index.isnumeric() != True) or ((int(index) >= 0) != True):
+        print("Invalid input! please try again")
+        return getIndex()
+
+    if (int(index) >= len(data)):
+        print("Input is out of range!\nInput should be <",len(data))
+        return getIndex()
+
+    else:
+        index = int(index)
+        return index
+
 
 
 
@@ -42,7 +142,7 @@ import requests
 ###################################################################
 ###################################################################
 def createJsonFile():
-    tabs_list = [{"Title":"SE Factory", "URL":"https://sefactory.webflow.io/", "nested":[]}, {"Title":"Google", "URL":"https://google.com/", "nested":[]}]
+    tabs_list = [{"Title":"SE Factory", "URL":"https://sefactory.webflow.io/", "nested":[{"Title":"SE Factory01", "URL":"https://sefactory.webflow.io/"},{"Title":"SE Factory02", "URL":"https://sefactory.webflow.io/"}]}, {"Title":"Google", "URL":"https://google.com/", "nested":[{"Title":"Google02", "URL":"https://google.com/"},{"Title":"Google03", "URL":"https://google.com/"}]}]
     json_string = json.dumps(tabs_list, indent=4)
     with open("opened_tabs.json", "w") as f:
         f.write(json_string)
@@ -132,107 +232,32 @@ def displayTabContent(index):
             data = json.load(f)
             url = data[index]["URL"]
             page = requests.get(url)
-            BeautifulSoup(page.text, "html")
-
-
-
-
-
-
-###################################################################
-###################################################################
-########### This function displays the software menu ##############
-###################################################################
-###################################################################
-def displayMenu():
-    print("Welcome to ABTS.\nThe most Advanced Browser Tabs Simulation.\nHere is the Menu, please choose wisely:\n1. Open Tab\n2. Close Tab\n3. Switch Tab\n4. Display All Tabs\n5. Open Nested Tabs\n6. Clear All Tabs\n7. Save Tabs\n8. Import Tabs\n9. Exit ABTS")
-    choice = input("Please enter your choice's number: ")
-    while (choice != "1") and (choice != "2") and (choice != "3") and (choice != "4") and (choice != "5") and (choice != "6") and (choice != "7") and (choice != "8") and (choice != "9"):
-        choice = input("Invalid input! Please enter one of the numbers below: ")
-    choice = int(choice)
-    return choice
-
+            html_soup = BeautifulSoup(page.text, "html.parser")
+            print(html_soup.prettify()) 
 
 
 
 
 ###################################################################
 ###################################################################
-###### This function asks the user to enter a tab's title, ########
-########## handles the user's input and return a title ############
+###      This function will get the titles of the parents       ###
+###            and the nested tabs and display them             ###
 ###################################################################
 ###################################################################
-def getTitle():
-    title = input("Please enter the tab's title: ")
-    print("Are you sure that",title,"is the tab's title?\n1. YES\n2. NO")
-    confirm = input("Please type 1 or 2: ")
-    while (confirm != "1") and (confirm != "2"):
-        confirm = input("Invalid input! Please enter 1 or 2: ")
-    if confirm == "1":
-        return title
-    elif confirm == "2":
-        return getTitle()
-
-
-
-
-
-###################################################################
-###################################################################
-###### This function asks the user to enter the tab's URL, ########
-############ handles the user's input and return a URL ############
-############################### ## ################################
-##  NOTE: I did a research to know how to check if the user's    ##
-##  input is a valid URL. I found so many methods and finally    ##
-##  chose .startwith() method. Here is the link of the page:    ###
-##    https://www.geeksforgeeks.org/python-check-url-string/     ##
-###################################################################
-###################################################################
-def getURL():
-    url = input("Please enter a valid URL.\nNote: URL should start with 'http:', 'https:' or 'www.'\nType your URL here: ")
-    while (url.startswith("https:") != True) and (url.startswith("http:") != True) and (url.startswith("www.") != True):
-        url = input("Invalid URL!\nPlease enter a valid URL.\nNote: URL should start with 'http:', 'https:' or 'www.'\nType your URL here: ")
-    print("Are you sure that",url,"is the tab's URL?\n1. YES\n2. NO")
-    confirm = input("Please type 1 or 2: ")
-    while (confirm != "1") and (confirm != "2"):
-        confirm = input("Invalid input! Please enter 1 or 2: ")
-    if confirm == "1":
-        return url
-    elif confirm == "2":
-        return getURL()
-
-
-
-
-
-###################################################################
-###################################################################
-########## This function asks the user to enter an Index, #########
-########## handles the user's input and return the Index ##########
-###                 If no index is provided,                    ###
-###         the function will return (-1) as a value            ###
-###################################################################
-###################################################################
-def getIndex():
-    with open("opened_tabs.json") as f:
+def printAllTitles():
+    with open("opened_tabs.json", "r") as f:
         data = json.load(f)
+        for i in data:
+            print(i["Title"])
+            for nested_tabs in i["nested"]:
+                if len(nested_tabs) > 0:
+                    print("   Nested tab >>> "+nested_tabs["Title"])
+            print("-----------")
 
-    index = input("Please enter the index: ")
-    if (index == ""):
-        index = -1
-        return index
 
-    if (index.isnumeric() != True) or ((int(index) >= 0) != True):
-        print("Invalid input! please try again")
-        return getIndex()
 
-    if (int(index) >= len(data)):
-        print("Input is out of range!\nInput should be <",len(data))
-        return getIndex()
 
-    else:
-        index = int(index)
-        return index
+
 
 
 
@@ -265,12 +290,15 @@ def main():
         updateJsonFile(updates)
         print("Tab was successfully closed!")
 
-    if choice == 3:
+    if choice == 3:  #3. Switch Tab
+        index = getIndex()
+        displayTabContent(index)
+
+    if choice == 4:  #4. Display All Tabs
+        printAllTitles()
 
 
 
-
-
-#main()
+main()
 
 
