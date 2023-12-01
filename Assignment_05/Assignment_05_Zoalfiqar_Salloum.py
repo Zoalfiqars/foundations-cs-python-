1bc1a9791a2b3a65dbd7ccfb10d6c98a747fc872
Zo Alfiqar Salloum <zworkmail2023@gmail.com>
2023-12-01 17:52:33 +0200

    Update Assignment_05_Zoalfiqar_Salloum.py

--- Assignment_05/Assignment_05_Zoalfiqar_Salloum.py
+++ Assignment_05/Assignment_05_Zoalfiqar_Salloum.py
@@ -3,3 +3,35 @@
 ########   Social Media Platform  #########
 ###########################################
 ###########################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def displayMenu():
+    print(">>>>   Welcome to SMP   <<<<")
+    print("1. Add new user to the platform.\n2. Remove User from the platform.\n3. Send a friend request.\n4. Remove a friend from your list\n5. View your list of friends.\n6. View the list of users on the platform.\n7. Exit")
+    choice = input("Please choose a number: ")
+    while (choice != "1") and (choice != "2") and (choice != "3") and (choice != "4") and (choice != "5") and (choice != "6") and (choice != "7"):
+        choice = input("Invalid input! Please choose one of the shown numbers: ")
+    choice = int(choice)
+    return choice
+
